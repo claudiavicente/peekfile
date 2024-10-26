3065502826f73951de18509261ecf6f53b35becb
Clàudia Vicente <02claudiavicentecomorera@gmail.com>
2024-10-26 19:04:11 +0200

    modify

--- countlines.sh
+++ countlines.sh
@@ -1,3 +1,1 @@
-NLINES=$( cat $1 | wc -l)
-if [[ $NLINES -gt 1 ]]; then echo $1 has $NLINES lines; elif [[ $NLINES -eq 1 ]]; then echo $1 has $NLINES line; else echo $1 is empty; fi
-
+for file in $@; do NLINES=$( cat $file | wc -l); if [[ $NLINES -gt 1 ]]; then echo $file has $NLINES lines; elif [[ $NLINES -eq 1 ]]; then echo $file has $NLINES line; else echo $file is empty; fi; done
