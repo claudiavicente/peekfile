76687033b8a9aec0044cfbe0cad03fa6e8e27793
Clàudia Vicente <02claudiavicentecomorera@gmail.com>
2024-11-19 10:14:00 +0100

    modify fastascan

--- fastascan.sh
+++ fastascan.sh
@@ -1,41 +1,60 @@
 
 # ARGUMENTS SETTING
-if [[ -z $1 ]]; then X="."; echo "! No folder specified as the first argument. Default: $X"; else X=$1; fi # set of folder X where to search files
-if [[ -z $2 ]]; then N="0"; echo -e "! No number of lines specified as the second argument. Default: $N \n"; else N=$2; fi # set of number N of lines
+# set of folder X where to search files:
+if [[ -z $1 ]]; then X="."; echo "[INFO] No folder specified as the first argument. Default: $X"; else X=$1; fi
 
-FILES=$(find $X -type f -name "*.fa" -or -name "*.fasta")
-if [[ -z $FILES ]]
-	then echo "No fa/fasta files found in $X"
-else
-	# REPORT
-	echo "========== SUMMARY OF FA/FASTA FILES IN $X AND SUBDIRECTORIES =========="
-	echo "-> Total fa/fasta files found: $(echo $FILES | wc -w)"
-	echo -e "-> Unique fasta IDs found: $(grep -h '^>' $FILES | awk '{print $1}' | sort | uniq | wc -l) \n"
+# set of number N of lines
+if [[ -z $2 ]]; then N="0"; echo "[INFO] No number of lines specified as the second argument. Default: $N"; else N=$2; fi 
 
+if [[ -d $X ]]; then # control: is X a directory?
+	echo
+	FILES=$(find $X -type f -name "*.fa" -or -name "*.fasta") 
+	if [[ -z $FILES ]]; then # control: there are fa/fasta files in the directory?
+		echo "[ERROR] No fa/fasta files found in $X"
+	else
+		# REPORT
+		echo "========== SUMMARY OF FA/FASTA FILES IN $X AND SUBDIRECTORIES =========="
+		echo "-> Total fa/fasta files found: $(echo $FILES | wc -w)"
+		echo "-> Unique fasta IDs found: $(awk '/>/{print $1}' $FILES | sort | uniq | wc -l)"
+		
 
-	for FILE in $FILES
+		for FILE in $FILES; do echo
+			if [[ -s $FILE ]]; then # control: is FILE empty?
 
-		# HEADER
-		do if [[ -h $FILE ]]; then SYM="SYMLINK"; else SYM="NOT SYMLINK"; fi # symbolic link check
-		NSEQ=$(grep '^>' $FILE | wc -l) # number of sequences
+				# HEADER
+				
+				# symbolic link check:
+				if [[ -h $FILE ]]; then SYM="SYMLINK"; else SYM="NOT SYMLINK"; fi 
 
-		SEQ=$(grep -v '^>' $FILE | sed 's/[- ]//g' | tr -d '\n') # grep lines that do not start by >, deleting "-", \n and spaces
-		LEN=$(echo -n $SEQ | wc -c) # count characters from $SEQ (total sequence length)
-		# TYPE=  file of amino acids or nucleotides check
+				# count lines starting by > (number of sequences):
+				NSEQ=$(awk '/>/{nseq+=1}END{print nseq}' $FILE) # $(grep '>' $FILE | wc -l) wc adds a lot of spaces in output
+				
+				##	
+				SEQ=$(awk '!/>/{gsub(/[ -]/, "",$0); seq = seq $0}END{print seq}' $FILE) 
+				###
 
-		echo "========== SUMMARY OF $FILE: $SYM, Nº SEQUENCES: $NSEQ, TOTAL SEQUENCE LENGTH: $LEN, $TYPE =========="
+				# count characters from sequence lines deleting - and spaces (total sequence length):
+				LEN=$(echo $SEQ | awk '{len+=length($0)}END{print len}') 
 
-		# FILE CONTENT
-		if [[ $N != 0 ]] 
-			then if [[ $(cat $FILE | wc -l) -le $((2*$N)) ]]
-				then cat $FILE
-				else head -n $N $FILE && echo … && tail -n $N $FILE
+				# file of amino acids or nucleotides check:
+				TYPE=$(if echo $SEQ | grep -q "[^AaTtGgCcUuNn]"; then echo "Aminoacids file"; else echo "Nucleotides file"; fi) # !!!!!!-m 1 in grep?			
+
+				echo "========== SUMMARY OF $FILE: $SYM, Nº SEQUENCES: $NSEQ, TOTAL SEQUENCE LENGTH: $LEN, $TYPE =========="
+
+				# FILE CONTENT
+				if [[ $N != 0 ]]; then
+					if [[ $(cat $FILE | wc -l) -le $((2*$N)) ]]; then cat $FILE; echo
+					else head -n $N $FILE; echo …; tail -n $N $FILE; echo
+					fi
+
+				else continue
+				fi
+
+			else echo -e "[WARNING] $FILE is empty \n"
 			fi
-		else continue
-		fi
-	done
+		done
+	fi
+else echo "[ERROR] $X is not an existing directory"
 fi
 
-
-### CONTROLS: is a file empty?, does $X exist? (add messages for controls????), Ns in nucleotide file?
-### TYPE!!
+### CONTROLS: file permissions??? , Ns in nucleotide file?, fasta headers????, what if N is a negative number?
